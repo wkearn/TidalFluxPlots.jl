8baeb8937349681db81db90f244ddb1cda5f164b
Will Kearney <wkearn@gmail.com>
2017-04-10 16:28:07 -0400

    Exercise all the other plotting recipes

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,3 +9,21 @@ deps = parse_deps(creek)
 ad1 = load_data(deps[1])
 
 plot(ad1)
+
+cs = parse_cs(creek)
+csd = load_data(cs)
+
+# Test the cross-section plot
+plot(csd)
+
+h1,Q1 = computedischarge(ad1,csd)
+
+# Test a few Quantities
+plot(h1)
+plot(Q1)
+
+cals = parse_cals(creek)
+cald = load_data(cals[1])
+
+plot(cald)
+
